103af2a287cc154efe45a53db72e129cba947329
Shadowfacts <me@shadowfacts.net>
2016-08-19 09:42:34 -0400

    Initial commit

---
+++ src/main/java/net/fabricmc/template/FabricTemplate.java
@@ -0,0 +1,12 @@
+package net.fabricmc.template;
+
+import net.fabricmc.base.loader.Init;
+
+public class FabricTemplate {
+
+	@Init
+	public void init() {
+		System.out.println("Hello from FabricTemplate");
+	}
+
+}
